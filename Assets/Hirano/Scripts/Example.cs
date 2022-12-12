f4819c2c0c61347c322fa90a686d0fa3afec6204
2PC-19\student <20cge0007kr@edu.sendai-com.ac.jp>
2022-12-12 14:50:26 +0900

    hp effect

---
+++ Assets/Hirano/Scripts/Example.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Coffee.UIExtensions;
+
+public class Example : MonoBehaviour
+{
+    public ShinyEffectForUGUI m_shiny;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+        while()
+        {
+
+        
+        m_shiny.Play(2);
+    }
+}
