5ef1b456890039402c5f7badb7024a96c6193ede
MurakamiRyota <20cge0011st@edu.sendai-com.ac.jp>
2022-12-19 15:52:40 +0900

    カメラに向きとポジションのプロパティを追加

--- Assets/Murakami/Scripts/PlayerC.cs
+++ Assets/Murakami/Scripts/PlayerC.cs
@@ -61,11 +61,6 @@ public class PlayerC : MonoBehaviour
         mouseX = Input.GetAxis("Mouse X");
         mouseY = Input.GetAxis("Mouse Y");
 
-        if (Mathf.Abs(mouseX) > 0.008f || Mathf.Abs(mouseY) > 0.005f)
-        {
-            Roll(mouseX, mouseY);
-        }
-
         rb.AddForce(H,0,V);
 
         //ジャンプ
@@ -86,12 +81,6 @@ public class PlayerC : MonoBehaviour
         {
             GameOver();
         }
-    }
-
-    public void Roll(float x, float y)
-    {
-        transform.RotateAround(this.transform.position, Vector3.up, x);
-        transform.RotateAround(this.transform.position, Vector3.up, y);
     }
 
     //ゲームオーバー処理
