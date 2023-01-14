4881d82393ba962b69524786240c6a80ae44fbe2
sca <21cge0020mr@edu.sca.ac.jp>
2023-01-14 13:13:58 +0900

    item

---
+++ Assets/Murakami/Scripts/UI関係/Itemcounter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Itemcounter : MonoBehaviour
+{
+    [SerializeField] private Image[] numberImage;
+    private int itemCon;
+    private Image image;
+    //private totalGameManager gm;
+    SpriteRenderer sr;
+
+    private void Awake()
+    {
+        //gm = FindObjectOfType<totalGameManager>();
+        //sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //itemCon = gm.PlayerIC;
+        if(Input.GetKey(KeyCode.Q)) itemCon++;
+        image =  numberImage[itemCon];
+    }
+}
