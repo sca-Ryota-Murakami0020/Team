e7f208195c9a5ba04e0fb46ee12415ce28545ecb
MurakamiRyota <20cge0011st@edu.sendai-com.ac.jp>
2023-01-16 18:25:51 +0900

    save3

--- Assets/Murakami/Scripts/タイム関係/SecondSImage.cs
+++ Assets/Murakami/Scripts/タイム関係/SecondSImage.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SecondSImage : MonoBehaviour
 {
+    [SerializeField] private Sprite[] numberImage;
+    private int secondSCount;
+    [SerializeField] private Image image;
+    private totalGameManager gm;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = FindObjectOfType<totalGameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        secondSCount = Mathf.FloorToInt(gm.TotalTime % 10);
+        image.sprite = numberImage[secondSCount];
     }
 }
